4314531328e9cf1500ad2c6da7f7ec6767437295
matt stone <mattstone22133@gmail.com>
2018-02-17 14:52:31 -0500

    Updated the QSorting instruction tutorial to complete the entire array. Still needs some of the instructions uncommented out, but this is a good commit spot.

--- core/src/enigma/engine/sorting/TutorialManagerQSort.java
+++ core/src/enigma/engine/sorting/TutorialManagerQSort.java
@@ -27,6 +27,7 @@ public class TutorialManagerQSort extends TutorialManager {
 	private final String endHighPtrCmd = cmdSymbol + "endHighPtrStage";
 	private final String swapPtrsCmd = cmdSymbol + "swapPtrs";
 	private final String repositionPvtCmd = cmdSymbol + "placebackPivot";
+	private final String baseCaseSelectCmd = cmdSymbol + "bcase";
 	
 	//state variables
 	private boolean waitForCachePivot = false;
@@ -39,6 +40,8 @@ public class TutorialManagerQSort extends TutorialManager {
 	private boolean waitEndHighPtr = false;
 	private boolean waitPtrSwap = false;
 	private boolean repositionPvt = false;
+	private boolean waitForBaseComplete = false;
+	private IndexInterval cachedFrameForBaseCase = null;
 	
 	
 	
@@ -69,7 +72,7 @@ public class TutorialManagerQSort extends TutorialManager {
 		createInstructions();
 		array.overrideInstructionTo(instructions.get(instructionIdx));
 		
-		array.setToggleSoltionSolver(false);
+		array.setToggleSolutionSolver(false);
 		array.togglePromptUpdates(false);
 	}
 	
@@ -81,104 +84,104 @@ public class TutorialManagerQSort extends TutorialManager {
 		//instructions.add(String.format("Press the %s button again to close the dialog.", GlobalStrings.showKeybindsButton));
 		
 		// Quicksort introduction
-		instructions.add("Quicksort is among the fastest sorting algorithms");
-		instructions.add("The first step of Quicksort is to 'partition' the array.");
-		instructions.add("To partition, we select some pivot element within the array.");
-		instructions.add("Different quicksort variants choose the pivot differently.");
+//		instructions.add("Quicksort is among the fastest sorting algorithms");
+//		instructions.add("The first step of Quicksort is to 'partition' the array.");
+//		instructions.add("To partition, we select some pivot element within the array.");
+//		instructions.add("Different quicksort variants choose the pivot differently.");
 		instructions.add("We'll just use the middle element.");
 		instructions.add(selectPivotCmd);
 		instructions.add("Click on the middle element to make it the pivot.");
 		
 		
-		instructions.add("Now, the purple element is now our pivot.");
-		instructions.add("Our goal is to figure something out about the pivot...");
-		instructions.add("We want to figure out it's correct place in the sorted array.");
-		instructions.add("So, the center might be the wrong (sorted) pivot spot...");
-		instructions.add("We can figure out the pivot's correct spot by 'partitioning'.");
-		
-		instructions.add("So, what exactly is 'partitoning the array'?");
-		instructions.add("Well, basically it's re-arranging elements based on the pivot.");
-		instructions.add("We want elements smaller than the pivot to the left...");
-		instructions.add("...and we want elements bigger than pivot to the right.");
-		
-		instructions.add("Before partitioning, we need to move the pivot out of the way.");
-		instructions.add("Basically, we're hiding the pivot until we know where it goes.");
-		instructions.add("Different quicksort variants hide the pivot in different ways...");
-		instructions.add("We'll just tuck it away at the start of our array sequence.");
+//		instructions.add("Now, the purple element is now our pivot.");
+//		instructions.add("Our goal is to figure something out about the pivot...");
+//		instructions.add("We want to figure out it's correct place in the sorted array.");
+//		instructions.add("So, the center might be the wrong (sorted) pivot spot...");
+//		instructions.add("We can figure out the pivot's correct spot by 'partitioning'.");
+//		
+//		instructions.add("So, what exactly is 'partitoning the array'?");
+//		instructions.add("Well, basically it's re-arranging elements based on the pivot.");
+//		instructions.add("We want elements smaller than the pivot to the left...");
+//		instructions.add("...and we want elements bigger than pivot to the right.");
+//		
+//		instructions.add("Before partitioning, we need to move the pivot out of the way.");
+//		instructions.add("Basically, we're hiding the pivot until we know where it goes.");
+//		instructions.add("Different quicksort variants hide the pivot in different ways...");
+//		instructions.add("We'll just tuck it away at the start of our array sequence.");
 		
 		instructions.add(cachePivotCmd);
 		instructions.add("Drag the pivot to the first element, and release to swap.");
-		instructions.add("So, the pivot's hidden -- but what are those arrow 'pointers'?");
-		instructions.add("The pointers are our place holders while we partition.");
-		
-		instructions.add("In regards to their colors...");
-		instructions.add("Let's say that anything 'red' is 'smaller' than the pivot...");
-		instructions.add("...and anything blue is bigger than the pivot.");
-		instructions.add("So: red = smaller; blue = bigger.");
-		
-		instructions.add("Now, we're going to 'color-code' elements based on size"); //maybe shouldn't say this
-		instructions.add("Let's move red pointer right marking red (smaller) elements");
+//		instructions.add("So, the pivot's hidden -- but what are those arrow 'pointers'?");
+//		instructions.add("The pointers are our place holders while we partition.");
+//		
+//		instructions.add("In regards to their colors...");
+//		instructions.add("Let's say that anything 'red' is 'smaller' than the pivot...");
+//		instructions.add("...and anything blue is bigger than the pivot.");
+//		instructions.add("So: red = smaller; blue = bigger.");
+//		
+//		instructions.add("Now, we're going to 'color-code' elements based on size"); //maybe shouldn't say this
+//		instructions.add("Let's move red pointer right marking red (smaller) elements");
 		instructions.add(show1LowPtrScanCmd);
 		instructions.add("Like that!");
-		instructions.add("The next element is also smaller than the pivot...");
-		instructions.add("...since it is smaller, it should be red too.");
+//		instructions.add("The next element is also smaller than the pivot...");
+//		instructions.add("...since it is smaller, it should be red too.");
 		instructions.add(show1LowPtrScanCmd);
 		instructions.add(moveLowPtrOnceCmd);
 		instructions.add("You try, click the next element to cycle colors.");
-		instructions.add("Don't actually think of this as color coding...");
-		instructions.add("What we're really doing is sliding the red pointer.");
-		instructions.add("We're sliding it over smaller elements...");
-		instructions.add("...searching for a larger element.");
-		instructions.add("The red pointer's element is larger than the purple pivot...");
-		instructions.add("Since it is larger, we should label it blue.");
+//		instructions.add("Don't actually think of this as color coding...");
+//		instructions.add("What we're really doing is sliding the red pointer.");
+//		instructions.add("We're sliding it over smaller elements...");
+//		instructions.add("...searching for a larger element.");
+//		instructions.add("The red pointer's element is larger than the purple pivot...");
+//		instructions.add("Since it is larger, we should label it blue.");
 		instructions.add(endLowPtrCmd);
 		instructions.add("Continue clicking on the element until it is blue.");
 		
-		instructions.add("Here's where we change things up...");
-		instructions.add("...now we want to slide the blue pointer over larger elements.");
-		instructions.add("Essentially, it's looking for a smaller-than-pivot element");
-		instructions.add("Since the element at the blue pointer is larger...");
+//		instructions.add("Here's where we change things up...");
+//		instructions.add("...now we want to slide the blue pointer over larger elements.");
+//		instructions.add("Essentially, it's looking for a smaller-than-pivot element");
+//		instructions.add("Since the element at the blue pointer is larger...");
 		instructions.add(moveHighPtrOnceCmd);
 		instructions.add("... it needs to be blue; please cycle its colors to blue.");
-		instructions.add("Now the blue pointer has found a smaller element!");
+//		instructions.add("Now the blue pointer has found a smaller element!");
 		instructions.add(endHighPtrCmd);
 		instructions.add("Please label it red, since it is smaller than the pivot.");
 		
-		instructions.add("The moment we've been waiting for! ...");
-		instructions.add("...the two pointers have found their targets.");
-		instructions.add("The next step is to swap the elements at the two pointers! ");
-		instructions.add("Swapping corrects the relative position of the elements...");
-		instructions.add("...we're trying to get all red on the left, and all blue on the right.");
+//		instructions.add("The moment we've been waiting for! ...");
+//		instructions.add("...the two pointers have found their targets.");
+//		instructions.add("The next step is to swap the elements at the two pointers! ");
+//		instructions.add("Swapping corrects the relative position of the elements...");
+//		instructions.add("...we're trying to get all red on the left, and all blue on the right.");
 		instructions.add(swapPtrsCmd);
 		instructions.add("Swap the two pointer's elements (click and drag).");
 		
-		instructions.add("Did you notice the pointers update?");
-		instructions.add("The red pointer moved right, the blue moved left.");
-		
-		instructions.add("Now what? Well, in this case we re-position the pivot.");
-		instructions.add("But normally, we will repeat blue and red pointer scanning.");
-		instructions.add("Normally, we scan again until the pointers cross.");
-		instructions.add("But only when the pointers cross each other...");
-		instructions.add("...do we reposition the pivot into the array.");
-		
-		instructions.add("Now then, since our pointers crossed over each other...");
-		instructions.add("...we're ready to position the pivot!");
-		instructions.add("We need to swap the pivot into it's correct place...");
-		instructions.add("...and the correct place is at one of the pointers.");
-		instructions.add("Can you guess which pointer we will swap with the pivot?");
-		instructions.add("Since we want all red on the left, and all blue on the right...");
-		instructions.add("We will swap with red element currently at the blue pointer...");
-		instructions.add("We do this because we hid the pivot on the left (red) side.");
+//		instructions.add("Did you notice the pointers update?");
+//		instructions.add("The red pointer moved right, the blue moved left.");
+//		
+//		instructions.add("Now what? Well, in this case we re-position the pivot.");
+//		instructions.add("But normally, we will repeat blue and red pointer scanning.");
+//		instructions.add("Normally, we scan again until the pointers cross.");
+//		instructions.add("But only when the pointers cross each other...");
+//		instructions.add("...do we reposition the pivot into the array.");
+//		
+//		instructions.add("Now then, since our pointers crossed over each other...");
+//		instructions.add("...we're ready to position the pivot!");
+//		instructions.add("We need to swap the pivot into it's correct place...");
+//		instructions.add("...and the correct place is at one of the pointers.");
+//		instructions.add("Can you guess which pointer we will swap with the pivot?");
+//		instructions.add("Since we want all red on the left, and all blue on the right...");
+//		instructions.add("We will swap with red element currently at the blue pointer...");
+//		instructions.add("We do this because we hid the pivot on the left (red) side.");
 		instructions.add(repositionPvtCmd);
 		instructions.add("Swap the pivot with the blue pointer element, please.");
 		
-		instructions.add("We've now completed our first partition!");
-		instructions.add("Quicksort is a recursive algorithm, ...");
-		instructions.add("... which means we recurse(repeat) on smaller sub-problems");
-		instructions.add("In this case, we have two smaller sub-problems, ...");
-		instructions.add("...our two subproblems are beside our last pivot!");
-		instructions.add("So, let's repeat and partition on both sides of the old pivot.");
-		
+//		instructions.add("We've now completed our first partition!");
+//		instructions.add("Quicksort is a recursive algorithm, ...");
+//		instructions.add("... which means we recurse(repeat) on smaller sub-problems");
+//		instructions.add("In this case, we have two smaller sub-problems, ...");
+//		instructions.add("...our two subproblems are beside our last pivot!");
+//		instructions.add("So, let's repeat and partition on both sides of the old pivot.");
+//		
 		instructions.add(selectPivotCmd);
 		instructions.add("Starting with the right half, pick the pivot (middle) element.");
 		
@@ -193,21 +196,29 @@ public class TutorialManagerQSort extends TutorialManager {
 		
 		instructions.add("This time we don't need to swap the pointers...");
 		instructions.add("...the blue pointer has already passed the red pointer");
-		instructions.add("Now, just correctly position pivot into the subarray.");
+		instructions.add("But algorithms are rigid, they cannot just skip steps...");
+		instructions.add("...the algorithm must check if it should do a swap.");
+		instructions.add("So remember, even though we see that no swap is needed...");
+		instructions.add("...the algorithm still has a step which checks for swapping.");
 		instructions.add(repositionPvtCmd);
+		instructions.add("Now then, correctly position pivot into the subarray.");
 		
 		instructions.add("Now we only have 1 element to pick a pivot from...");
-		instructions.add("...which means this element is already correctly positioned");
+		instructions.add("...which means this element is already correctly positioned.");
 		instructions.add("This is what we call a base case...");
 		instructions.add("Base cases are essentially end the of recursion.");
+		instructions.add(baseCaseSelectCmd);
+		instructions.add("Let's go ahead and mark this one as correctly positioned.");
 		
 		instructions.add("Now we only have two elements...");
 		instructions.add("...which is another base case!");
-		instructions.add("Again, We don't need to recursively partition anymore.");
+		instructions.add("Again, we don't need to recursively partition anymore.");
 		instructions.add("We only need to swap the two if they're out of order.");
+		instructions.add(baseCaseSelectCmd);
 		instructions.add("Since these two are out of order, let's swap the elements..");
 		
 		instructions.add("Again we're presented with the two element base case.");
+		instructions.add(baseCaseSelectCmd);
 		instructions.add("They're out of order, so let's swap.");
 		instructions.add("And we have it, the entire array has been sorted!");
 		
@@ -297,11 +308,11 @@ public class TutorialManagerQSort extends TutorialManager {
 				return true;
 			}
 			else if (instruction.equals(show1LowPtrScanCmd)) {
-				array.setToggleSoltionSolver(true);
+				array.setToggleSolutionSolver(true);
 				
 				array.nextSolveStep(false);
 				
-				array.setToggleSoltionSolver(false);
+				array.setToggleSolutionSolver(false);
 				return true;
 			}
 			else if (instruction.equals(moveLowPtrOnceCmd)) {
@@ -334,14 +345,19 @@ public class TutorialManagerQSort extends TutorialManager {
 			else if (instruction.equals(repositionPvtCmd)) {
 				QuickSortableArray.Stage stage = array.getStage();
 				while(stage == Stage.SWAP) {
-					array.setToggleSoltionSolver(true);
-					array.nextSolveStep(false);
+					array.setToggleSolutionSolver(true);
+					array.attemptSwapStep(true);
 					stage = array.getStage();
-					array.setToggleSoltionSolver(false);
+					array.setToggleSolutionSolver(false);
 				}
 				
 				allowArrayIO = true;
 				repositionPvt = true;
+				return true;
+			}else if (instruction.equals(baseCaseSelectCmd)) {
+				allowArrayIO = true;
+				waitForBaseComplete = true;
+				cachedFrameForBaseCase = array.peekCurrentStackFrame();
 				return true;
 			}
 			
@@ -413,7 +429,14 @@ public class TutorialManagerQSort extends TutorialManager {
 				allowArrayIO = false;
 				nextInstructionText();
 			}
-		} 
+		} else if (waitForBaseComplete) {
+			//when the person is on another stack frame, they've finished the base case.
+			if(array.peekCurrentStackFrame() != cachedFrameForBaseCase) {
+				waitForBaseComplete = false;
+				allowArrayIO = false;
+				nextInstructionText();
+			}
+		}
 		
 	}
 
