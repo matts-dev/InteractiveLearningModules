f6fe645c421498ccf71906d681f5b47219900b27
Matt Stone <mattstone22133@gmail.com>
2018-02-24 10:38:59 -0500

    refactored naming in main module from being specific to theory to something more generic.

---
+++ core/src/enigma/engine/GameMainModule.java
@@ -0,0 +1,80 @@
+package enigma.engine;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+import enigma.engine.floatingpoint.MasterFloatingPointModule;
+import enigma.engine.fsm.MasterFSMModule;
+import enigma.engine.regex.MasterRegularExpressionModule;
+import enigma.engine.sorting.QSortInstructionModule;
+import enigma.engine.sorting.QSortPracticeModule;
+
+//@formatter:off
+enum module
+{
+	REGEX,
+	FSM,
+	FLOAT_POINT,
+	QSORT_INSTRUCTION,
+	QSORT_PRACTICE,
+	INSERT_SORT_INSTRUCTION,
+	INSERT_SORT_PRACTICE,
+	SELECTION_SORT_INSTRUCTION,
+	SELECTION_SORT_PRACTICE,
+	BINARY	
+}
+//@formatter:on
+
+public class GameMainModule extends CourseModule {
+	public static final module compileTimeModuleSetting = module.QSORT_PRACTICE;
+	public GameMainModule(OrthographicCamera camera) {
+		super(camera);
+
+		switch (GameMainModule.compileTimeModuleSetting) {
+		case REGEX:
+			subModules.add(new MasterRegularExpressionModule(camera)); //regular expression module
+			break;
+		case FSM:
+			subModules.add(new MasterFSMModule(camera));
+			break;
+		case FLOAT_POINT:
+			subModules.add(new MasterFloatingPointModule(camera));
+			break;
+		case QSORT_INSTRUCTION:
+			subModules.add(new QSortInstructionModule(camera));
+
+			break;
+		case QSORT_PRACTICE:
+			subModules.add(new QSortPracticeModule(camera));
+
+			break;
+		case SELECTION_SORT_INSTRUCTION:
+
+			break;
+		case SELECTION_SORT_PRACTICE:
+
+			break;
+		case INSERT_SORT_INSTRUCTION:
+
+			break;
+		case INSERT_SORT_PRACTICE:
+
+			break;
+		case BINARY:
+			
+			break;
+		default:
+			break;
+		}
+		
+
+		// load the first module
+		loadCurrentModule();
+	}
+
+	@Override
+	public void logic() {
+		super.logic();
+	}
+}
+
+
